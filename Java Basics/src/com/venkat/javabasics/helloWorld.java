6377d61c68f8c82e1c09f17116dfbc1903717f02
Siva Mani Venkat K <ksmvenkat5799@gmail.com>
2025-01-26 20:26:45 -0600

    Initial Commit.

---
+++ Java Basics/src/com/venkat/javabasics/helloWorld.java
@@ -0,0 +1,18 @@
+package com.venkat.javabasics;
+
+class Helper{
+	
+	public String print() {
+		return "Hello, I am Venkat, practincing Java!!";
+	}
+}
+
+public class helloWorld {
+	
+	public static void main(String[] args) {
+		
+		Helper help = new Helper();
+		System.out.println(help.print());
+		
+	}
+}
